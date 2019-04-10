929782c497aa071b8d3d7f2a211e1e912dac65d9
cooperren <493252378@qq.com>
2019-04-10 14:15:15 +0800

    update version

--- init.el
+++ init.el
@@ -1,11 +1,18 @@
+;;; init.el --- Load the full configuration -*- lexical-binding: t -*-
+;;; Commentary:
 
-;;; This file bootstraps the configuration, which is divided into
-;;; a number of other files.
+;; This file bootstraps the configuration, which is divided into
+;; a number of other files.
 
-(let ((minver "24.1"))
+;;; Code:
+
+;; Produce backtraces when errors occur
+(setq debug-on-error t)
+
+(let ((minver "24.4"))
   (when (version< emacs-version minver)
     (error "Your Emacs is too old -- this config requires v%s or higher" minver)))
-(when (version< emacs-version "24.4")
+(when (version< emacs-version "25.1")
   (message "Your Emacs is old, and some functionality in this config will be disabled. Please upgrade if possible."))
 
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
@@ -14,20 +21,23 @@
 (defconst *spell-check-support-enabled* nil) ;; Enable with t if you prefer
 (defconst *is-a-mac* (eq system-type 'darwin))
 
-
 ;;----------------------------------------------------------------------------
-;; Temporarily reduce garbage collection during startup
+;; Adjust garbage collection thresholds during startup, and thereafter
 ;;----------------------------------------------------------------------------
-(defconst sanityinc/initial-gc-cons-threshold gc-cons-threshold
-  "Initial value of `gc-cons-threshold' at start-up time.")
-(setq gc-cons-threshold (* 128 1024 1024))
-(add-hook 'after-init-hook
-          (lambda () (setq gc-cons-threshold sanityinc/initial-gc-cons-threshold)))
+(let ((normal-gc-cons-threshold (* 20 1024 1024))
+      (init-gc-cons-threshold (* 128 1024 1024)))
+  (setq gc-cons-threshold init-gc-cons-threshold)
+  (add-hook 'emacs-startup-hook
+            (lambda () (setq gc-cons-threshold normal-gc-cons-threshold))))
 
 ;;----------------------------------------------------------------------------
 ;; Bootstrap config
 ;;----------------------------------------------------------------------------
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(setq url-proxy-services
+   '(("no_proxy" . "^\\(localhost\\|10.*\\)")
+     ("http" . "proxy.tencent.com:8080")
+     ("https" . "proxy.tencent.com:8080")))
 (require 'init-utils)
 (require 'init-site-lisp) ;; Must come before elpa, as it may provide package.el
 ;; Calls (package-initialize)
@@ -43,11 +53,8 @@
 ;;----------------------------------------------------------------------------
 ;; Load configs for specific features and modes
 ;;----------------------------------------------------------------------------
-
-(require-package 'wgrep)
-(require-package 'project-local-variables)
 (require-package 'diminish)
-(require-package 'scratch)
+(maybe-require-package 'scratch)
 (require-package 'command-log-mode)
 
 (require 'init-frame-hooks)
@@ -55,7 +62,11 @@
 (require 'init-themes)
 (require 'init-gui-frames)
 (require 'init-dired)
-;(require 'init-flycheck)
+(require 'init-isearch)
+(require 'init-grep)
+(require 'init-uniquify)
+(require 'init-ibuffer)
+(require 'init-flycheck)
 
 (require 'init-recentf)
 (require 'init-smex)
@@ -64,17 +75,15 @@
 (require 'init-company)
 (require 'init-windows)
 (require 'init-sessions)
-(require 'init-fonts)
+(require 'init-mmm)
 
 
 (require 'init-editing-utils)
 (require 'init-whitespace)
-(require 'init-fci)
-
 
 (require 'init-vc)
 (require 'init-darcs)
-(require 'init-git)
+;(require 'init-git)
 (require 'init-github)
 
 (require 'init-projectile)
@@ -87,12 +96,14 @@
 (require 'init-javascript)
 
 (require 'init-org)
-(require 'init-python-mode)
+(require 'init-python)
 
 (require 'init-sql)
+(require 'init-yaml)
+(maybe-require-package 'nginx-mode)
 
 (require 'init-paredit)
-(require 'init-lisp)
+;(require 'init-lisp)
 (require 'init-slime)
 
 
@@ -126,9 +137,11 @@
 ;;----------------------------------------------------------------------------
 ;; Allow access from emacsclient
 ;;----------------------------------------------------------------------------
-(require 'server)
-(unless (server-running-p)
-  (server-start))
+;(add-hook 'after-init-hook
+;          (lambda ()
+;            (require 'server)
+;            (unless (server-running-p)
+;              (server-start))))
 
 
 ;;----------------------------------------------------------------------------
@@ -139,24 +152,22 @@
 
 
 ;;----------------------------------------------------------------------------
-;; Allow users to provide an optional "init-local" containing personal settings
-;;----------------------------------------------------------------------------
-(require 'init-local nil t)
-
-
-;;----------------------------------------------------------------------------
 ;; Locales (setting them earlier in this file doesn't work in X)
 ;;----------------------------------------------------------------------------
 (require 'init-locales)
 
 
-(when (maybe-require-package 'uptimes)
-  (add-hook 'after-init-hook (lambda () (require 'uptimes))))
+;;----------------------------------------------------------------------------
+;; Allow users to provide an optional "init-local" containing personal settings
+;;----------------------------------------------------------------------------
+(require 'init-local nil t)
+
+
 
 (provide 'init)
 
 ;; Local Variables:
-(setq-default indent-tabs-mode nil)
 ;; coding: utf-8
 ;; no-byte-compile: t
 ;; End:
+;;; init.el ends here
